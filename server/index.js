043fed7aec61dc065dae8778679eb2cb82100e6c
unknown <ks8004208135@gmail.com>
2023-12-16 20:04:47 +0530

    AI Image Generator

---
+++ server/index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+ import connectDB from './mongodb/connect.js';
+ import postRoutes from './routes/postRoutes.js';   //after creating mongo db
+ import dalleRoutes from './routes/dalleRoutes.js';   //we created routes
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+
+  app.use('/api/v1/post', postRoutes);
+  app.use('/api/v1/dalle', dalleRoutes);
+
+app.get('/', async (req, res) => {
+  //res.status(200).json({
+res.send('Hello  World');
+    //message: 'Hello from DALL.E!',
+  })
+// });
+
+const startServer = async () => {
+ try {
+    connectDB(process.env.MONGODB_URL);
+  //   app.listen(8080, () => console.log('Server has started on port http://localhost:8080'))
+  //  }
+  app.listen(8080, () =>{ console.log('Server has started on port:8080')})
+}
+
+catch (error) {
+    console.log(error);
+  }
+  }
+
+startServer();
